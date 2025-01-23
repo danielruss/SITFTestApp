0f48a83281682b6d7d8beb10d10502bc7c533fab
Daniel Russ <druss@mail.nih.gov>
2025-01-23 14:11:47 -0500

    Initial vesrion

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SITFTestApp
-Example application that uses SITF
+Example application that uses *SOCcer in the Field*
+
+SOCcer in the Field (SITF) is typically called during the questionnaire process. For example, after asking the study participant about their job title and job task, SITF is a API to a server-based version of SOCcer, (currently SOCcer 2.0) that returns a JSON object
+
+There is only one method
+
+### HTTP request
+
+`GET <API_Root>/soccer/code?title=<JobTitle>&task=<JobTask>&n=<n_codes>`
+
+
+where `API_ROOT` is the URL of your SITF instance, `JobTitle` is a single job title, `JobTask` is a single job task, and `n_codes` is the number of US SOC 2010 codes returned by SITF.
+
+I have included a web-app that simulates the what the interaction between the questionnaire software and the SITF API.  You need to have your instance of SITF running.  Unfortunately cloud providers charge per instance, so I cannot just give you my API_ROOT.  However, I can help you get up and running.
---
+++ index.js
@@ -0,0 +1,60 @@
+import localforage from 'https://cdn.jsdelivr.net/npm/localforage@1.10.0/+esm'
+
+let cache = await localforage.createInstance({
+    name: "sitftestapp",
+    store: "cache"
+})
+
+let cached_apiroot = await cache.getItem("apiroot") ?? ""
+let api_url = "";
+console.log("... in index.js ...", cached_apiroot)
+
+function updateDisplay() {
+    let display = (cached_apiroot.length == 0) ? "none" : "initial"
+    console.log(`in ud: api:   ${cached_apiroot} lenght:${cached_apiroot.length} ${display}`)
+    document.querySelectorAll("body > div").forEach(e => {
+        console.log(`${e.id} set to ${display}`)
+        e.style.display = display
+    })
+    display = (cached_apiroot.length == 0) ? "initial" : "none"
+    document.querySelectorAll("body > div[data-noroot]").forEach(e => {
+        let jobTitle = document.getElementById("JobTitle").value
+        let jobTask = document.getElementById("JobTask").value
+        let n = parseInt(document.getElementById("nCodes").value) ?? 4
+
+        if (jobTitle.length > 0 && jobTask.length > 0) {
+            api_url = `${cached_apiroot}/soccer/code?title=${jobTitle}&task=${jobTask}&n=${n}`
+        } else if (jobTitle.length > 0) {
+            api_url = `${cached_apiroot}/soccer/code?title=${jobTitle}&n=${n}`
+        } else if (jobTask.length > 0) {
+            api_url = `${cached_apiroot}/soccer/code?title=${jobTask}&n=${n}`
+        }
+
+        if (api_url.length > 0) {
+            document.getElementById("apiurl").innerText = api_url
+        } else {
+            document.getElementById("apiurl").innerText = `${cached_apiroot}/soccer/code?title=<JobTitle>&task=<JobTask>&n=<N>`
+        }
+        console.log(`${e.id} set to ${display}`)
+        e.style.display = display
+    })
+}
+
+updateDisplay()
+document.getElementById("apirootButton").addEventListener("click", async () => {
+    let apiroot = document.getElementById("apiroot").value
+    await cache.setItem("apiroot", apiroot)
+    cached_apiroot = await cache.getItem("apiroot") ?? ""
+    updateDisplay()
+})
+
+document.getElementById("runButton").addEventListener("click", async () => {
+    updateDisplay()
+    console.log("click....", api_url)
+    let json = await (await fetch(api_url)).json()
+    let text = JSON.stringify(json, null, 2)
+    document.getElementById('json_out').innerText = text
+    console.log("text ===> ", text)
+})
+
+console.log("... finished index.js ...")
